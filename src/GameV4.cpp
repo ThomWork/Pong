5c4d7995d1e950fcc38854a2f587acd501636478
ThomWork <thomas.workman.tw@gmail.com>
2022-04-26 16:57:58 +0100

    Updates

--- src/GameV4.cpp
+++ src/GameV4.cpp
@@ -1,8 +1,8 @@
 #pragma once
 
-#include "SFML/Graphics.hpp"
-#include "SFML/System.hpp"
-#include "SFML/Window.hpp"
+#include "../libs/SFML/Graphics.hpp"
+#include "../libs/SFML/System.hpp"
+#include "../libs/SFML/Window.hpp"
 #include "PuckV4.cpp"
 #include "PaddleV4.cpp"
 
